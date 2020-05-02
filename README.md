b975c6ada086bd1c71459971117d01a4e0d08a2d
Matthew Harris <simonh241@yahoo.com>
2020-05-01 21:02:55 -0700

    Version 0.3 -- see README.md

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ basic project set up mostly completed, still need to get API keys for the majori
 # Version 0.2
 Although searches are not yet implemented, LIRI successfully detects too many or too few arguments, invalid arguments, and displays appropriate error messages to the user.
 A "help" command has been added to display messages that explain how to use LIRI.
+
+# Version 0.3
+Added code to detect the need to use the defaults for the 'spotify-this-song'command. modified argument error detection to allow 'spotify-this-song' to have no 2nd argument, and to use the hard-coded defaults, per the instructions.
+Added code to populate the Verb (command) and Item (what to search for) in the case that Liri is run with the 'spoitfy -this-song' command an no song argument.
+Added the code to carry out the "do-what-it-says" command (read from the random.tx file)
+Added stubbed procedures for all search commands. (next will be to begin implementing searches).
--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
+var FS = require("fs");
 require("dotenv").config();
 chalk = require("chalk");
 var keys = require("./keys.js");
 
+var Verb =""; // global variable for the command to perform
+var Item =""; // global variable for the item to search for
 
 //THE FOLLOWING STATEMENT PRODUCES A RUNTIME ERROR, "Spotify is not defined."
 // var spotify = new Spotify(keys.Spotify);  //this line may not work until the keys.js file is populated more fully.
@@ -18,10 +21,21 @@ var cmdCount = process.argv.length;
 var cmdIn = process.argv.slice(2);
 console.log("count of args after slice: " + cmdIn.length);
 
-//check for too few arguments
+//check for too few arguments -- NOTE: IF THE COMMAND IS "SPOTIFY-THIS" and there is no song argument, then default to "The Sign" by Ace of Base
+
 if (cmdIn.length == 1){
   let s = cmdIn[0].toLowerCase();
-  if (s != "do-what-it-says" && s != "help") {
+  //do we need to set the default for 'spotify-this'?
+  if (s == "spotify-this-song") {
+    //yes, set default values
+    console.log(chalk.yellow.bold("NEED TO SET SPOTIFY DEFAULT"));
+    SetSpotifyDefaults();
+  }
+
+  //do we need to read the text file to get default command and item?
+
+
+  if (s != "do-what-it-says" && s != "help" && s != "spotify-this-song") {
     BadArgumentsMsg("ERROR!! except for the commands \'do-what-it-says\' and \'help\' Liri expects at least 2 arguments! Please try again!");
   return;
 }
@@ -36,8 +50,13 @@ if (cmdIn.length > 2) {
   return;
  }
 
-// console.log("count of process args: " + process.argv.length);
+ //if we get here, the arguments are not too many or too few.
+ ParseArguments(); //call function to parse out command and search item.
+ console.log(chalk.yellow.bold("Verb is: " + Verb));
+ console.log(chalk.yellow.bold("Item is: " + Item))
 
+// console.log("count of process args: " + process.argv.length);
+function ParseArguments() {
 var searchname = process.argv.slice(2);
 process.argv.slice(2);
 
@@ -49,10 +68,10 @@ process.argv.slice(2);
 for (i=0; i < cmdIn.length; i++) {
   console.log("loop count: " + i); 
   if (i==0) {
-    var Verb = searchname[i];
+    Verb = searchname[i];
   }
   else {
-    var Item = searchname[i];
+    Item = searchname[i];
   }
   console.log("argument: " + searchname[i]);
 }
@@ -61,6 +80,10 @@ console.log("command verb: " + Verb);
 console.log("item to act on: " + Item);
 
 executeCommand(Verb,Item);
+console.log("command verb: " + Verb);
+console.log("item to act on: " + Item);
+}
+
 
 function executeCommand(cmd,item) {
   //this function determinse whether the command is valid, and executes the corresponding API call to get information.
@@ -70,15 +93,20 @@ function executeCommand(cmd,item) {
     switch (cmd) {
       case "concert-this":
         console.log(chalk.yellow.bold("search bands in town for: " + item));
+        SearchBandsInTown(item);
         break;
       case "spotify-this-song":
         console.log(chalk.yellow.bold("search spotify for: " + item));
+        SearchSpotify(item);
         break;
       case "movie-this":
         console.log(chalk.yellow.bold("search omdb for: " + item));
+        SearchOMDB(item);
         break;
       case "do-what-it-says":
         console.log(chalk.yellow.bold("do the spotify search in the random.txt file"));
+        ReadFromFile();
+        SearchSpotify(item);
         break;
       case "help":
         UserHelp();
@@ -109,7 +137,7 @@ function UserHelp() {
   console.log(chalk.blue.bold("the \'concert-this\' command causes Liri to search Bands in Town for upcoming shows by a performing artist or group that you specify"));
   console.log(chalk.blue.bold("The \'spotify-this-song\' command causes Liri to search Spotify for a song that you specify"));
   console.log(chalk.blue.bold("The \'movie-this\' command causes Liri to search OMDB to get information about a movie that you specify."));
-  console.log(chalk.blue.bold("The \do-what-it-says command causes Liri to read a command and name from a file named \'random.txt\' and execute the command for the name from the file. By default the file is set to search spotify for the song \"I want it that way\""));
+  console.log(chalk.blue.bold("The \'do-what-it-says\' command causes Liri to read a command and name from a file named \'random.txt\' and execute the command for the name from the file. By default the file is set to search spotify for the song \"I want it that way\""));
   console.log(chalk.blue.bold("The \'help\' command displays this help"));
  
 }
@@ -126,4 +154,59 @@ function BadArgumentsMsg(msg) {
    console.log(chalk.yellow.bold("run Liri with the command Help to get information about what each Liri command does."));
    console.log("");
    console.log(chalk.green.bold("run 'node liri help\' to get more help using Liri."));
+}
+
+function SetSpotifyDefaults() {
+  // this function sets the default values for when the command is 'spotify-this' and no song argument is supplied
+  console.log(chalk.yellow.bold("SETTING SPOTIFY DEFAULT"));
+  Verb = "spotify-this-song";
+  Item = "The Sign";
+}
+
+function ReadFromFile() {
+  let inputArray = [];
+  console.log(chalk.yellow.bold("READ THE TEXT FILE TO GET COMMAND AND ITEM"));
+
+FS.readFile("random.txt", "utf8", function(error,data) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log(chalk.blue.bold("[" + data + "]"));
+  data = data.replace(/\n/g,"");  //get rid of LF char
+  data = data.replace(/\r/g,"");  //get rid of CR
+
+
+  inputArray = data.split(",");
+  console.log(chalk.yellow.bold("length of input array: " + inputArray.length));
+
+  //assume that the input array has 2 elements and element 0 is the verb, and element 1 is the item.
+  Verb = inputArray[0];
+  Item = inputArray[1]  //.rep
+  // let s = inputArray[1].replace(/\r/,"");
+  // Item = s;
+  // Item=Item.replace(/\r/g,"");
+
+  console.log(chalk.blue.bold("Verb is: [" + Verb + "]"));
+  console.log(chalk.blue.bold("item is: [" + Item + "]"));
+})
+
+}
+
+function SearchSpotify(verb,item) {
+  // this function searches spotify for the specified item.
+  console.log(chalk.yellow.bold("FUNCTION TO SEARCH SPOTIFY FOR: " + Item));
+
+}
+
+function SearchBandsInTown(item) {
+  //THIS function searches bands in town for the specified item.
+  console.log(chalk.yellow.bold("FUNCTION TO SEARCH BANDS IN TOWN FOR: " + item));
+  
+
+}
+
+function SearchOMDB(item) {
+  console.log(chalk.yellow.bold("FUNCTION TO SEARCH OMDB FOR: " + item));
+
+
 }
